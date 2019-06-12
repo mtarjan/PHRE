d320aa3eb387984f51f19379cf7001f7c5c60b0e
mtarjan <max.tarjan@gmail.com>
2019-06-11 20:11:58 -0700

    updating hyena code

--- PHRE_hyena.R
+++ PHRE_hyena.R
@@ -8,3 +8,8 @@
 ##smoother: bandwidth value (matric or scalar) or 'default'
 ##percent: percent kernel to calculate (e.g., 90)
 
+##prepare inputs
+data<-read.csv("C:/Users/max/Desktop/Tarjan/hyena_data/OHB09.csv")
+plot(data$LAT, data$LON)
+
+fence<-rgdal::readOGR(dsn = "C:/Users/max/Desktop/Tarjan/hyena_data", layer="ONR_shape")
