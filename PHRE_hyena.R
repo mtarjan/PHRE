eabeab2d4a056024505ec201a3d4a55938eaff51
mtarjan <max.tarjan@gmail.com>
2019-06-11 19:53:23 -0700

    added phre function

---
+++ PHRE_hyena.R
@@ -0,0 +1,10 @@
+##PHRE in hyenas
+##Prepared by M Tarjan
+##June 11, 2019
+
+##INPUTS
+##locs: dataframe with colnames x & y
+##rast: raster with 1 or more layers
+##smoother: bandwidth value (matric or scalar) or 'default'
+##percent: percent kernel to calculate (e.g., 90)
+
