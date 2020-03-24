d059eff69ab11a91427a79e66676a48057636772
mtarjan <max.tarjan@gmail.com>
2020-03-24 16:22:04 -0700

    added sample otter locations

--- PHRE_seaotter.R
+++ PHRE_seaotter.R
@@ -2,8 +2,8 @@
 ##Max Tarjan; ltarjan@ucsc.edu
 ##July 29, 2014
 
-##load data; specify file paths to supporting information 9 data
-#data<-read.csv("~/Dropbox/S9_File/BSR/F4_994_1346_data.xls"); data<-cbind(data$Xcoord, data$Ycoord)
+##load data; specify file paths to supporting information
+data<-read.csv("F4_994_1346.csv"); data<-cbind(data$Xcoord, data$Ycoord)
 grid<-read.csv("C:/Users/max/Downloads/journal.pone.0150547.s009.csv")
 data<-subset(grid, TealeY<-160000 & TealeY > -180000, select=c("TealeX", "TealeY"))
 data<- data[sample(x = 1:nrow(data), size = 200),]##create random points in range of grid for locations
@@ -18,15 +18,18 @@ rasters[[2]]<-rasterFromXYZ(cbind(grid$TealeX, grid$TealeY, log(grid$Distance+0.
 smoother<-cbind(c(1,0), c(0,0.3)) #(ATOS, 0) (0, log(dist))
 
 ##adapt the ATOS smoothing parameter based on the nearest neighbor distances between re-sight locations
-library(amap)
-dist<-as.matrix(Dist(data))
-nn<-dim(0)
-for (c in 1:dim(dist)[2]) {
-  min.temp<-min(dist[which(dist[,c]!=0),c])
-  nn<-c(nn,min.temp)
-}
-mean.nn<-mean(log(nn))
-smoother[1,1]<-smoother[1,1]*2*(mean.nn/4)^2.5
+#library(amap)
+#dist<-as.matrix(Dist(data))
+#nn<-dim(0)
+#for (c in 1:dim(dist)[2]) {
+#  min.temp<-min(dist[which(dist[,c]!=0),c])
+#  nn<-c(nn,min.temp)
+#}
+#mean.nn<-mean(log(nn))
+#smoother[1,1]<-smoother[1,1]*2*(mean.nn/4)^2.5
+
+##load the phre function
+source("phre_function.R")
 
 ##apply phre function
 HR<-phre(locs=data, rast=rasters, smooth=smoother, percent=90)
